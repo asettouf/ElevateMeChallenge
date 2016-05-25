43c20a954b286cca2088c9d161d36f821900e495
Adonis Settouf <settouf@ds1.internal>
2016-05-25 15:13:58 +0200

    First commit

---
+++ elvt.js
@@ -0,0 +1,46 @@
+{
+    init: function(elevators, floors) {
+        var elevatorFirst = elevators[0];
+        var elevatorSecond = elevators[1];
+        var elevatorThird = elevators[2];
+        var elevatorFourth = elevators[3];
+        elevators.forEach(function(elevator, i){
+            
+			elevator.on("floor_button_pressed", function(floorNum){
+                this.goToFloor(floorNum);
+            });
+        });
+
+        floors.forEach(function(floor, i){
+            floor.on("up_button_pressed", function(){
+                elevators.forEach(function(elevator, i){
+                    if(!elevator.destinationQueue || elevator.loadFactor == 1  || elevator.destinationQueue.indexOf(floor.floorNum) > -1){
+                        return;
+                    }
+                    elevator.goToFloor(floor.floorNum);
+					return false;
+
+                });
+            });
+			floor.on("down_button_pressed", function(){
+                elevators.forEach(function(elevator, i){
+                    if(!elevator.destinationQueue || elevator.loadFactor == 1 || elevator.destinationQueue.indexOf(floor.floorNum) > -1){
+                        return;
+                    }
+                    elevator.goToFloor(floor.floorNum);
+					return false;
+
+                });
+            });
+        });
+
+    },
+        update: function(dt, elevators, floors) {}
+}
+
+
+
+
+
+
+
