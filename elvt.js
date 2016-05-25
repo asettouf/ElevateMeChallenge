5c1e7ef4b424774c34f6f160bde34cfe018a211d
Adonis Settouf <settouf@ds1.internal>
2016-05-25 16:28:22 +0200

    Refining model

--- elvt.js
+++ elvt.js
@@ -12,7 +12,20 @@
                 this.goToFloor(floorNum);
             });
         });
-
+		var removeDuplicate = function(a) {
+			var seen = {};
+			var out = [];
+			var len = a.length;
+			var j = 0;
+			for(var i = 0; i < len; i++) {
+				 var item = a[i];
+				 if(seen[item] !== 1) {
+					   seen[item] = 1;
+					   out[j++] = item;
+				 }
+			}
+			return out;
+		};
         floors.forEach(function(floor, i){
             floor.on("up_button_pressed", function(){
                 var diff = 0;
@@ -20,14 +33,17 @@
 				var elevatorToSend = 0;
 				var that = this;
                 elevators.forEach(function(elevator, i){
-                    diff = Math.abs(that.floorNum() - elevator.currentFloor());
+                    diff = Math.abs(elevator.destinationQueue[0] - elevator.currentFloor());
 					if(diff < minDiff && this.loadFactor != 1){
 						elevatorToSend = i;
 						minDiff = diff;
 						
 					}
                 });
-				elevators[elevatorToSend].goToFloor(this.floorNum());
+				elevators[elevatorToSend].destinationQueue.splice(1,0, this.floorNum());
+				elevators[elevatorToSend].destinationQueue = removeDuplicate(elevators[elevatorToSend].destinationQueue);
+				elevators[elevatorToSend].checkDestinationQueue();
+				//elevators[elevatorToSend].goToFloor(this.floorNum());
             });
 			floor.on("down_button_pressed", function(){
 				var diff = 0;
@@ -35,18 +51,29 @@
 				var elevatorToSend = 0;
 				var that = this;
                 elevators.forEach(function(elevator, i){
-                    diff = Math.abs(that.floorNum() - elevator.currentFloor());
+                    diff = Math.abs(elevator.destinationQueue[0] - elevator.currentFloor());
 					if(diff < minDiff && this.loadFactor != 1){
 						elevatorToSend = i;
 						minDiff = diff;
 					}
                 });
-				elevators[elevatorToSend].goToFloor(this.floorNum());
+				//console.log(this.floorNum());
+				//console.log(elevators[elevatorToSend].destinationQueue);
+				elevators[elevatorToSend].destinationQueue.splice(1,0, this.floorNum());
+				elevators[elevatorToSend].destinationQueue = removeDuplicate(elevators[elevatorToSend].destinationQueue);
+				//console.log(elevators[elevatorToSend].destinationQueue);
+				elevators[elevatorToSend].checkDestinationQueue();
+				//elevators[elevatorToSend].goToFloor(this.floorNum());
             });
         });
 
     },
-        update: function(dt, elevators, floors) {}
+        update: function(dt, elevators, floors) {
+			elevators.forEach(function(elevator, i){
+				console.log("elevator " + i + " current Queue: " + elevator.destinationQueue);
+            });
+			
+		}
 }
 
 
